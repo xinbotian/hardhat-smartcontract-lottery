5cdb32e79f54ea51b2d44bd266e74c68ee69471f
Xinbo Tian <xinbot@yahoo.com>
2022-09-13 13:12:49 -0700

    update

--- contracts/test/VRFCoordinatorV2Mock.sol
+++ contracts/test/VRFCoordinatorV2Mock.sol
@@ -1,4 +1,4 @@
-// SPDX-LICENSE-Identifier:MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import"@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
